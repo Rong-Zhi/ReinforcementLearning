7472bda82f81db4c41fb6e0546e5229600d95863
Rong-Zhi <zhirong1993@yahoo.com>
2018-02-20 14:48:57 +0100

    TRPO

--- TRPO.py
+++ TRPO.py
@@ -1,27 +1,34 @@
 import gym
 import numpy as np
+import collections
+from collections import defaultdict
 import sklearn.pipeline
 import tensorflow as tf
 from sklearn.kernel_approximation import RBFSampler
 
 env = gym.envs.make('Cart-Pole-v0')
 
-observe_samples = np.array([env.observation_space.sample() for i in range(10000)])
+def single_path(env, policy, path_length, discount_factor):
+    Transition = collections.namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
+    Q = defaultdict(lambda: np.zeros(env.action_space.n))
+    V = defaultdict(lambda: np.zeros(e))
+    state = env.reset()
+    samples = []
+    for t in range(path_length):
+        action_probs = policy(state)
+        action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
+        next_state, reward, done, _ = env.step(action)
+        samples.append(Transition(state, action, reward, next_state, done))
+        state = next_state
+    t = 0
+    for i_sample in samples:
+        G = sum(r * (discount_factor) ** i for i, r in enumerate(i_sample.reward[t:]))
+        if t == 0:
+            eta_policy = G/path_length
+        Q[i_sample.state][i_sample.action] = G / (path_length - 1)
+        t += 1
 
-featurizer = sklearn.pipeline.FeatureUnion([
-    ('rbf1', RBFSampler(gamma=5, n_components=100)),
-    ('rbf2', RBFSampler(gamma=2, n_components=100)),
-    ('rbf3', RBFSampler(gamma=1, n_components=100)),
-    ('rbf4', RBFSampler(gamma=0.5, n_components=100))
-])
-featurizer.fit(observe_samples)
-
-def featurize_state(data):
-    featurized = featurizer.transform(data.reshape(1,env.observation_space.shape[0]))
-    return featurized[0]
-
-def single_path(env, path_num, path_length):
-    pass
+    return samples, Q, eta_policy
 
 def vine(env):
     pass
@@ -38,13 +45,24 @@ def conjugate_gradient(env):
 def policy_update(env):
     pass
 
-def TRPO(env,
+class Agent(object):
+    def __init__(self,
+         env,
          policy_iteration=200,
          stepsize=0.01,
          discount=0.99,
          path_num=50,
-         path_length=1000,
-         computation_time=5,
-         hidden_sizes = (32, 32)):
-    pass
+         path_length=500,
+         hidden_sizes = 30):
 
+        self.act_space = env.action_space.n
+        self.states = tf.placeholder(tf.float32, [env.observation_space[0]] ,name='states')
+        self.adv = tf.placeholder(dtype=tf.float32, name='advantages')
+        self.layer1 = tf.layers.dense(self.states, hidden_sizes)
+        self.output = tf.layers.dense(self.layer1, self.act_space)
+        self.action_prob = tf.nn.softmax(self.output)
+
+
+
+def trpo():
+    pass
