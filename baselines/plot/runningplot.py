5b66f74e32a6e28a01080f19be7454bb6b7a15a1
zhi <zhirong1993@yahoo.com>
2018-05-16 12:13:14 +0200

    update

--- baselines/plot/runningplot.py
+++ baselines/plot/runningplot.py
@@ -7,10 +7,12 @@ import os
 
 sns.set(color_codes=True)
 #
-# path3= '/home/zhi/Documents/ReinforcementLearning/tmp/LunarLanderContinuous-v2/ppo2-ent2-05-03-18-41-39/progress.csv'
-# path2 = '/home/zhi/Documents/ReinforcementLearning/tmp/LunarLanderContinuous-v2/ppo2-ent1-05-03-18-43-10/progress.csv'
-path1 = '/home/zhi/Documents/ReinforcementLearning/tmp/LunarLanderContinuousPOMDP-v0/ppo2-long-10ep-ent001-05-08-19-26-04/progress.csv'
-# path6 = '/home/zhi/Documents/ReinforcementLearning/tmp/RockSample/ppofullentretrace05001-5000-04-23-15-29-36/progress.csv'
+path1= '/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/clip-0-05-15-03-17/progress1.csv'
+path2 = '/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/clip-hist10-0-05-15-19-27/progress1.csv'
+path3 = '/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/clip-hist15-0-05-15-19-26/progress1.csv'
+path4= '/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/entropy001-0-05-15-03-12/progress1.csv'
+path5 = '/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/entropy001-hist10-0-05-15-19-30/progress1.csv'
+path6 = '/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/entropy001-hist15-0-05-15-19-32/progress1.csv'
 
 # path1 = '/Users/zhirong/Documents/Masterthesis-code/tmp/LunarLanderContinuous-v2/ppo2-ent-10ep-5runs-05-05-13-09-58/progress.csv'
 # path2 = '/Users/zhirong/Documents/Masterthesis-code/tmp/LunarLanderContinuous-v2/ppo2-15ep-5runs-05-06-00-23-44/progress1.csv'
@@ -32,14 +34,14 @@ def plots(i):
     for ax in axes:
         ax.clear()
     d1 = pd.read_csv(path1)
-    # d2= pd.read_csv(path2)
-    # d3 = pd.read_csv(path3)
-    # d4 = pd.read_csv(path4)
-    # d5 = pd.read_csv(path5)
-    # d6 = pd.read_csv(path6)
+    d2 = pd.read_csv(path2)
+    d3 = pd.read_csv(path3)
+    d4 = pd.read_csv(path4)
+    d5 = pd.read_csv(path5)
+    d6 = pd.read_csv(path6)
     # data = pd.concat([d3, d5])
-    data = d1
-    # data = pd.concat([d1, d2, d3, d4])
+    # data = d1
+    data = pd.concat([d1, d2, d3, d4, d5, d6])
 
     sns.tsplot(data=data, time='Iteration', value='EpRewMean',unit='trial', condition='Name', ax=axes[0], ci=95)
     # sns.tsplot(data=d1, time='Iteration', value='EpDRewMean',unit='trial', condition='Name', ax=axes[1])
