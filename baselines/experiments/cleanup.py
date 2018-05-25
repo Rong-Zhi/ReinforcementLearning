20e7e69f471a7acc1ae57e61422a3dc96b7c6a19
zhi <zhirong1993@yahoo.com>
2018-05-25 20:16:19 +0200

    udpate

--- baselines/experiments/cleanup.py
+++ baselines/experiments/cleanup.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import glob
 # path1 = '/Users/zhirong/Documents/share/LunarLanderContinuous-v2/ent-3l-0-05-21-19-36/progress.csv'
 # path2 = '/Users/zhirong/Documents/share/LunarLanderContinuous-v2/ent-3l-1-05-21-19-36/progress.csv'
 # path3 = '/Users/zhirong/Documents/share/LunarLanderContinuous-v2/ent-3l-2-05-21-19-36/progress.csv'
@@ -12,6 +12,10 @@ path2 = '/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/ent-dynammic01-
 path3 = '/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/ent-dynammic01-2l-hist20-bh5-batch32-2-05-23-15-51/progress.csv'
 path4 = '/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/ent-dynammic01-2l-hist20-bh5-batch32-3-05-23-15-51/progress.csv'
 path5 = '/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/ent-dynammic01-2l-hist20-bh5-batch32-4-05-23-15-51/progress.csv'
+
+
+path=r'/home/zhi/Documents/share/LunarLanderContinuousPOMDP-v0/'
+allfiles=glob.glob(path+'ent-dynammic01-2l-hist16-bh17-batch32-6-05-24-15-51')
 
 d1 = pd.read_csv(path1)
 d2 = pd.read_csv(path2)
